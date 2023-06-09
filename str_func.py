86b8474f01d8ed21eb1c3ac1c1eb002b471648d7
Dimanikin <driver-s@mail.ru>
2023-06-09 16:08:02 +0530

    add capital_first_letters function and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
+"""
+function returns capital letters
+"""
+
+
 def capital_letters(upstr):
     return upstr.upper()
+
+
+"""
+function returns first capital letter
+"""
+
+
+def capital_first_letters(title_str):
+    return title_str.title()
